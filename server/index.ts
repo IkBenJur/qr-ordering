55929ac41826279df2648dbb5bf957dfee599649
IkBenJur <jurspammer@gmail.com>
2024-06-22 17:08:37 +0200

    Added get tables route

--- server/index.ts
+++ server/index.ts
@@ -44,6 +44,12 @@ const tables: Table[] = [
 ];
 
 
+//Get tables
+app.get("/tables", (req, res) => {
+  return res.json({ tables: tables });
+});
+
+
 server.listen(3000, () => {
   console.log("server running on 3000");
 });
