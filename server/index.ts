24e5d4948838a1cf5d0a380a16c486105933ca23
IkBenJur <jurspammer@gmail.com>
2024-06-22 17:09:32 +0200

    Added create order route

--- server/index.ts
+++ server/index.ts
@@ -71,6 +71,44 @@ app.post("/table", (req, res) => {
   return res.status(200).json({ message: "Succesfully added new table" });
 });
 
+//Send order to table
+app.post("/order", (req, res) => {
+  const newOrder = req.body.order as Order;
+
+  if (!newOrder) {
+    console.log("Failed to find order in post");
+    return res.status(400).json({ error: "Order not found in request body" });
+  }
+
+  const tableIdIndex = tables.findIndex(
+    (table) => table.id == newOrder.tableId
+  );
+  if (tableIdIndex == -1) {
+    console.log("Table with id not found for order");
+    return res.status(422).json({ error: "No table with that id" });
+  }
+
+  const orderAlreadyAdded = tables[tableIdIndex].orders.find(
+    (order) => order.id == newOrder.id
+  );
+  if (orderAlreadyAdded) {
+    console.log("Order with that Id already on table");
+    return res.status(422).json({ error: "Order with that id on table" });
+  }
+
+  const itemIdsOnMenu = newOrder.itemIds.every((id) =>
+    menu.find((item) => item.id == id)
+  );
+  if (!itemIdsOnMenu) {
+    console.log("Item on order not present on menu");
+    return res.status(422).json({ error: "Item not found on menu" });
+  }
+
+  tables[tableIdIndex].orders.push(newOrder);
+  console.log("Order added to table");
+
+  return res.status(200).json({ message: "Added order to table" });
+});
 
 server.listen(3000, () => {
   console.log("server running on 3000");
